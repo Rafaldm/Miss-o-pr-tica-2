69b0e0db08f4d8e522654fc6cdf0c422e3937312
Rafaldm <140225101+Rafaldm@users.noreply.github.com>
2024-08-01 15:43:08 -0300

    m2

---
+++ estruturas_condicao2.py
@@ -0,0 +1,9 @@
+tempoExperiencia = 1
+if tempoExperiencia < 2:
+    print('Nível de conhecimento Júnior. ')
+
+elif tempoExperiencia >= 2:
+    print('Nível de conhecimento Pleno.')
+
+else:
+    print('Nível de conhecimento Sênior.')
