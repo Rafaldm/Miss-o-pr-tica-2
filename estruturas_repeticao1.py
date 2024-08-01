4fa6a9522b6f46d7f71760c0858f0a7012b41ba0
Rafaldm <140225101+Rafaldm@users.noreply.github.com>
2024-08-01 15:43:55 -0300

    m3

---
+++ estruturas_repeticao1.py
@@ -0,0 +1,5 @@
+entrada_idade = '';
+entrada_idade = input('Digite um número ou 0 para sair: \n')
+while entrada_idade != '0':
+    entrada_idade = input('Número digitado: \n')
+print('Você digitou 0, encerrado!')
