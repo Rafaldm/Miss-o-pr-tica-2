63a7ab496cb9d11c3fa4b48fe0153dc83bbfef17
Rafaldm <140225101+Rafaldm@users.noreply.github.com>
2024-08-01 15:44:22 -0300

    m4

---
+++ estruturas_repeticao2.py
@@ -0,0 +1,15 @@
+texto = 'Olá laço for.';
+for texto in range(1, 11):
+    if texto == 11:
+        continue
+    else:
+        print(texto)
+print('caracteres')
+
+
+for texto  in range(1, 11):
+    if texto % 2 == 0:
+        pass
+    else:
+        print(texto)
+print('Número de intervalo')
